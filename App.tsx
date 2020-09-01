4ca5d11fee4c2fff783afe0d063d3e0ab7937d02
Jay Steigner <ioctosteigner@gmail.com>
2020-09-01 18:43:55 -0500

    added logout button

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import GoogleSignIn from './GoogleSignIn';
+import GoogleLogIn from './client/components/GoogleLogIn';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <GoogleSignIn />
+      <GoogleLogIn />
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style='auto' />
     </View>
