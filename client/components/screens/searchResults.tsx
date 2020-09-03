b3d7e15bc626b68a31afcdf843ad7be32f587459
Sebastian Hove <sebastianhove@gmail.com>
2020-09-03 12:15:04 -0500

    "Added Search Results & Search buttons in respective files"

--- client/components/screens/searchResults.tsx
+++ client/components/screens/searchResults.tsx
@@ -5,9 +5,13 @@ export default function SearchResults({ navigation }) {
   const pressHandler = () => {
     navigation.navigate('TruckDetails');
   };
+  const searchHandler = () => {
+    navigation.navigate('Search');
+  };
   return (
     <View>
       <Text> Search Results Screen </Text>
+      <Button title='Go To Search Screen' onPress={searchHandler} />
       <Button title='Go To Truck Details' onPress={pressHandler} />
     </View>
   );
