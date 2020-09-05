27aa9c76ec5549e291f881cd65848b4b42ebc21e
Peter Klingelhofer <peterklingelhofer@gmail.com>
2020-09-05 17:14:51 -0500

    (add) modular InfoWindow and Thumbnail drop-ins

---
+++ client/components/dropIns/InfoWindow.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, View, Text, Image } from 'react-native';
+import Thumbnail from './Thumbnail';
+export default function InfoWindow({ currentTruck }) {
+  const { full_name, blurb, logo, star_average } = currentTruck;
+  return (
+    <View>
+      <Text>{`Rollin Fatties, ${full_name}`}</Text>
+      <Text>{`${blurb}`}</Text>
+      <Thumbnail logo={logo} />
+      <Text>{`${star_average}`}</Text>
+    </View>
+  );
+}
