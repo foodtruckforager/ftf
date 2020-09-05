a04788237bb94be606cb59a60d16091536286d51
Sebastian Hove <sebastianhove@gmail.com>
2020-09-05 13:54:16 -0500

    "(in progress): Passing data between components successfully"

--- client/components/screens/searchResults.tsx
+++ client/components/screens/searchResults.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import { StyleSheet, View, Text, Button } from 'react-native';
 
-export default function SearchResults({ navigation }) {
+export default function SearchResults({ navigation, value }) {
   const pressHandler = () => {
     navigation.navigate('TruckDetails');
   };
   const searchHandler = () => {
     navigation.navigate('Search');
   };
+  const value2 = navigation.getParam('value');
+  console.log(value2);
+
+  // spinner
+  // need to make a search query to server with
+  // value and then map data and replace spinner
   return (
     <View>
-      <Text> Search Results Screen </Text>
+      <Text> {value2} hellooo </Text>
+
       <Button title='Go To Search Screen' onPress={searchHandler} />
       <Button title='Go To Truck Details' onPress={pressHandler} />
     </View>
