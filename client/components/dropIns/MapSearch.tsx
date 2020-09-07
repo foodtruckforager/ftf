90f0a5bf8f5c886cfbbe3e7e4db1bc3410b1399f
Peter Klingelhofer <peterklingelhofer@gmail.com>
2020-09-07 12:34:53 -0500

    (add) added food genre search functionality to map tracker page

---
+++ client/components/dropIns/MapSearch.tsx
@@ -0,0 +1,19 @@
+import { SearchBar } from 'react-native-elements';
+import axios from 'axios';
+
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, Button, TextInput } from 'react-native';
+
+export default function MapSearch({ search, setSearch }) {
+
+  return (
+    <View>
+      <SearchBar
+        placeholder="Search Food Trucks..."
+        onChangeText={(text) => setSearch(text)}
+        value={search}
+        lightTheme={true}
+      />
+    </View>
+  );
+}
