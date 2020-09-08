6e126cb708e4e386afc8392dfb2ef4618faa4b25
Jay Steigner <ioctosteigner@gmail.com>
2020-09-08 16:48:19 -0500

    (fix) login more functional, logout still needs work

--- App.tsx
+++ App.tsx
@@ -1,32 +1,10 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import GoogleLogIn from './client/components/dropIns/GoogleLogIn';
-import RootDrawerNavigator from './client/components/routes/drawer';
-import TruckOwnerProfile from './client/components/screens/truckOwnerProfile';
 import LogIn from './client/components/screens/LogIn';
 
 export default function App() {
-  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
-  const [isTruckOwnerLoggedIn, setIsTruckOwnerLoggedIn] = useState(false);
-
   return (
     <LogIn />
-    // <>
-    //   <>
-    //     { (!isUserLoggedIn && !isTruckOwnerLoggedIn) && (
-    //     <GoogleLogIn
-    //       isUserLoggedIn={isUserLoggedIn}
-    //       setIsUserLoggedIn={setIsUserLoggedIn}
-    //       isTruckOwnerLoggedIn={isTruckOwnerLoggedIn}
-    //       setIsTruckOwnerLoggedIn={setIsTruckOwnerLoggedIn}
-    //     />
-    //     )}
-    //   </>
-    //   <>
-    //     { isUserLoggedIn && <RootDrawerNavigator /> }
-    //     { isTruckOwnerLoggedIn && <TruckOwnerProfile /> }
-    //   </>
-    // </>
   );
 }
 
