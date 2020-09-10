9501ff6a5cc60f4304888da9db5a974716f7d525
Christopher Stumpe <toph.stumpe@gmail.com>
2020-09-10 01:42:55 -0500

    (add) add basic badge functionality

--- client/components/dropIns/InfoWindow.tsx
+++ client/components/dropIns/InfoWindow.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, View, Text, Image, TouchableOpacity, Linking, Platform, } from 'react-native';
+// import Icon from 'react-native-vector-icons';
 import Thumbnail from './Thumbnail';
+import { Avatar, Badge, Icon, withBadge } from 'react-native-elements';
+// import axios from 'axios';
 
 const styles = StyleSheet.create({
   container: {
-    // paddingTop: 40,
-    // backgroundColor: '#ddd',
+    paddingTop: 10,
+    backgroundColor: '#FFFFFF',
   },
   topRow: {
     flexDirection: 'row',
@@ -24,11 +27,18 @@ const styles = StyleSheet.create({
   },
   blurb: {
     flexWrap: 'wrap',
+    backgroundColor: '#FFFFFF',
   },
   style: {
     paddingLeft: 10,
   },
+  badge: {
+    // position: 'absolute',
+    right: 10,
+    top: -10,
+  },
 });
+
 export default function InfoWindow({ currentTruck }) {
   const truncate = (elem: string, limit: number, after: string) => {
     if (!elem || !limit) return;
@@ -36,30 +46,43 @@ export default function InfoWindow({ currentTruck }) {
     content = `${content.slice(0, limit)}${after}`;
     return content;
   };
-  const { full_name, blurb, logo, star_average, phone_number, food_genre } = currentTruck;
+  const { full_name, blurb, logo, star_average, phone_number, food_genre, number_of_reviews } = currentTruck;
   // name, thumbnail, phone number, genre, distance, food pic, blurb, stars
   // for calling
   const tele = phone_number;
   // for display
-  
+
   // let teleDisplay = phone_number
   //   if (tele.length === 7) {
   //     teleDisplay = `1 (225) ${teleDisplay.slice(0, 3)}-${teleDisplay.slice(3, 6)}`;
   //   } else if (tele.length === 10) {
   //     teleDisplay = `1 (${teleDisplay.slice(0, 3)}) ${teleDisplay.slice(3, 6)}-${teleDisplay.slice(7)}`;
   //   }
-  
 
-  function dialCall() {
-    let phoneNumber = tele;
-    if (Platform.OS === 'android') {
-      phoneNumber = `tel:${phoneNumber}`;
-    } else {
-      phoneNumber = `telprompt:${phoneNumber}`;
+  // function dialCall() {
+  //   let phoneNumber = tele;
+  //   if (Platform.OS === 'android') {
+  //     phoneNumber = `tel:${phoneNumber}`;
+  //   } else {
+  //     phoneNumber = `telprompt:${phoneNumber}`;
+  //   }
+  //   Linking.openURL(phoneNumber);
+  // }
+  let openState = true;
+  const openBadge = () => {
+    if (openState) {
+      return (
+        <View>
+          <Badge value=" " status="success" />
+        </View>
+      );
     }
-    Linking.openURL(phoneNumber);
-  }
-
+    return (
+      <View>
+        <Badge value=" " status="error" />
+      </View>
+    );
+  };
   return (
     <View>
       <View
@@ -69,6 +92,9 @@ export default function InfoWindow({ currentTruck }) {
           style={styles.topRow}
         >
           <Thumbnail logo={logo} />
+          <View style={styles.badge}>
+            {openBadge()}
+          </View>
           <View
             style={styles.topRowText}
           >
@@ -78,23 +104,24 @@ export default function InfoWindow({ currentTruck }) {
               '',
             )}`}
             </Text>
+            {/* <Icon name="phone" size={30} color="#900" /> */}
             <Text>
-              {phone_number}
+              {String.fromCharCode(9990)}{phone_number}
             </Text>
             <Text>
-              {food_genre}
+              {food_genre.charAt(0).toUpperCase()}{food_genre.slice(1)}
             </Text>
-          <View style={styles.stars}>
-            <Text style={{ color: 'orange' }}>
-              {String.fromCharCode(9733).repeat(Math.floor(star_average))}
-            </Text>
-            <Text style={{ color: 'lightgrey' }}>
-              {String.fromCharCode(9733).repeat(5 - Math.floor(star_average))}
-            </Text>
-            <Text style={styles.reviews}>
-              #0 reviews
-            </Text>
-          </View>
+            <View style={styles.stars}>
+              <Text style={{ color: 'orange' }}>
+                {String.fromCharCode(9733).repeat(Math.floor(star_average))}
+              </Text>
+              <Text style={{ color: 'lightgrey' }}>
+                {String.fromCharCode(9733).repeat(5 - Math.floor(star_average))}
+              </Text>
+              <Text style={styles.reviews}>
+                {number_of_reviews} reviews
+              </Text>
+            </View>
           </View>
           <View>
             <Text>
