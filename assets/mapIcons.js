2de8514e9f2e538f6fd80aabda095a0c43482d9a
Peter Klingelhofer <peterklingelhofer@gmail.com>
2020-09-10 10:12:15 -0500

    (add) added default truck image for unknown food genre trucks

--- assets/mapIcons.js
+++ assets/mapIcons.js
@@ -2,6 +2,7 @@ const foodTruckSmall = require('./foodtruckstill86.png');
 
 const foodIcons = {
   google: foodTruckSmall,
+  undefined: foodTruckSmall,
   mexican:
     'http://www.myiconfinder.com/uploads/iconsets/64-64-ae3644852016062df814998796ff33ee.png',
   thai:
