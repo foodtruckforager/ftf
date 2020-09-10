02cd559c0359305e1e54deb59736c3e7965fa9af
Jay Steigner <ioctosteigner@gmail.com>
2020-09-10 14:35:33 -0500

    (add) TruckItem.rsx, working on TruckPosts

--- client/components/screens/truckPosts.tsx
+++ client/components/screens/truckPosts.tsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, Text, Button } from 'react-native';
+import axios from 'axios';
 
 export default function TruckPosts({ navigation }) {
+  const [currentTruckPosts, setCurrentTruckPosts] = useState('');
+
+  useEffect(() => {
+    axios.get(`${EXPO_LocalLan}/truck/truckpost/1`)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
   const pressHandler = () => {
     navigation.navigate('TruckReviews');
   };
   const pressHandlerDetails = () => {
     navigation.navigate('TruckDetails');
   };
+
   return (
-    <View style={StyleSheet.container}>
+    <View style={styles.container}>
       <Text> Truck Posts </Text>
       <Button title='Go To Reviews' onPress={pressHandler} />
       <Button title='Go To Details' onPress={pressHandlerDetails} />
