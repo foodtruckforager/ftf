443ad48d5ec41b9208949855886bec498553a84d
Peter Klingelhofer <peterklingelhofer@gmail.com>
2020-09-14 19:10:20 -0500

    (in progress) adding select location map to truck owner profile

---
+++ client/components/dropIns/LocationSelectMap.tsx
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Dimensions } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import { View } from '../themes/Themed';
+
+const { width, height } = Dimensions.get('window');
+const ASPECT_RATIO = width / height;
+const LATITUDE = 29.9990674;
+const LONGITUDE = -90.0852767;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
+export default function LocationSelectMap({
+  provider,
+  navigation,
+  latitude,
+  longitude,
+  setLatitude,
+  setLongitude,
+}) {
+  const [region, setRegion] = useState({
+    latitude: latitude || LATITUDE,
+    longitude: longitude || LONGITUDE,
+    latitudeDelta: LATITUDE_DELTA,
+    longitudeDelta: LONGITUDE_DELTA,
+  });
+  alert(latitude);
+  return (
+    <View style={styles.container}>
+      <MapView
+        provider={provider}
+        style={styles.map}
+        initialRegion={region}
+        zoomTapEnabled={false}
+        showsUserLocation={true}
+        followsUserLocation={false}
+      >
+        <View>
+          <Marker
+            coordinate={{
+              latitude: +latitude,
+              longitude: +longitude,
+            }}
+            draggable={true}
+            onDragEnd={(e) => {
+              console.log('dragEnd', e.nativeEvent.coordinate);
+            }}
+          />
+        </View>
+      </MapView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  customView: {
+    width: 280,
+    height: 140,
+  },
+  plainView: {
+    width: 60,
+  },
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  bubble: {
+    flex: 1,
+    backgroundColor: 'rgba(70,70,70,0.7)',
+    paddingHorizontal: 18,
+    paddingVertical: 12,
+    borderRadius: 20,
+  },
+  latlng: {
+    width: 200,
+    alignItems: 'stretch',
+  },
+  button: {
+    width: 80,
+    paddingHorizontal: 12,
+    alignItems: 'center',
+    marginHorizontal: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginVertical: 20,
+    backgroundColor: 'transparent',
+  },
+  calloutButton: {
+    width: 'auto',
+    backgroundColor: 'rgba(255,255,255,0.7)',
+    paddingHorizontal: 6,
+    paddingVertical: 6,
+    borderRadius: 12,
+    alignItems: 'center',
+    marginHorizontal: 10,
+    marginVertical: 10,
+  },
+});
--- client/components/screens/TruckOwnerProfile.tsx
+++ client/components/screens/TruckOwnerProfile.tsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import {
-  StyleSheet, View, Text, Button, Switch,
-} from 'react-native';
+import { StyleSheet, View, Text, Button, Switch } from 'react-native';
 import axios from 'axios';
+import LocationSelectionMap from '../dropIns/LocationSelectMap';
+import Constants from 'expo-constants';
 
 const TruckOwnerProfile = ({ navigation, route }) => {
   const [isEnabled, setIsEnabled] = useState(false);
@@ -21,8 +21,11 @@ const TruckOwnerProfile = ({ navigation, route }) => {
   const [longitude, setLongitude] = useState(null);
 
   useEffect(() => {
-    const getData = async() => {
-      await axios.get(`${process.env.EXPO_LocalLan}/truck/login/${route.params.googleId}`)
+    const getData = async () => {
+      await axios
+        .get(
+          `${process.env.EXPO_LocalLan}/truck/login/${route.params.googleId}`
+        )
         .then((response) => {
           setTruckName(response.data.full_name);
           setPhoneNumber(response.data.phone_number);
@@ -47,31 +50,53 @@ const TruckOwnerProfile = ({ navigation, route }) => {
 
   return (
     <View>
-      <Switch
-        trackColor={{ false: '767577', true: '#81b0ff' }}
-        thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
-        ios_backgroundColor="#3e3e3e"
-        onValueChange={toggleSwitch}
-        value={isEnabled}
-      />
-      <Text>{truckName}</Text>
-      <Button title="Edit" onPress={() => navigation.navigate('TruckOwnerProfileEdit')}> Click</Button>
-      <Button
-        title="Logout"
-        onPress={() => {
-          navigation.navigate('LogIn',
-            { previous_screen: 'LogOut' });
-        }}
-      />
+      <View style={styles.map}>
+        <LocationSelectionMap
+          latitude={latitude}
+          longitude={longitude}
+          navigation={navigation}
+          setLatitude={setLatitude}
+          setLongitude={setLongitude}
+        />
+      </View>
+      <View>
+        <Switch
+          trackColor={{ false: '767577', true: '#81b0ff' }}
+          thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+          ios_backgroundColor="#3e3e3e"
+          onValueChange={toggleSwitch}
+          value={isEnabled}
+        />
+      </View>
+      <View>
+        <Text>{truckName}</Text>
+        <Button
+          title="Edit"
+          onPress={() => navigation.navigate('TruckOwnerProfileEdit')}
+        >
+          Click
+        </Button>
+        <Button
+          title="Logout"
+          onPress={() => {
+            navigation.navigate('LogIn', { previous_screen: 'LogOut' });
+          }}
+        />
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 18,
+    flex: 1,
+  },
+  map: {
+    padding: 300,
+    paddingTop: Constants.statusBarHeight,
   },
 });
 
