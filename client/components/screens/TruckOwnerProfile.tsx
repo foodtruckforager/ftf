3e7d169c7bc3dd721493035a2679b65429b44ceb
Jay Steigner <ioctosteigner@gmail.com>
2020-09-14 20:11:36 -0500

    (fix) owner profile rerendering after update

--- client/components/screens/TruckOwnerProfile.tsx
+++ client/components/screens/TruckOwnerProfile.tsx
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from 'react';
 import {
   StyleSheet, View, Text, Button, Switch,
 } from 'react-native';
+import { useIsFocused } from '@react-navigation/native';
 import axios from 'axios';
 
 const TruckOwnerProfile = ({ navigation, route }) => {
-  // const [isEnabled, setIsEnabled] = useState(false);
   const [truckName, setTruckName] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
   const [qrCode, setQrCode] = useState('');
@@ -19,29 +19,37 @@ const TruckOwnerProfile = ({ navigation, route }) => {
   const [openStatus, setOpenStatus] = useState(false);
   const [latitude, setLatitude] = useState(null);
   const [longitude, setLongitude] = useState(null);
+  const [cameFromProfileEdit, setCameFromProfileEdit] = useState(false);
+
+  const isFocused = useIsFocused();
+
+  const getData = async() => {
+    await axios.get(`${process.env.EXPO_LocalLan}/truck/login/${route.params.googleId}`)
+      .then((response) => {
+        setTruckName(response.data.full_name);
+        setPhoneNumber(response.data.phone_number);
+        setQrCode(response.data.qrCode);
+        setlogo(response.data.logo);
+        setFoodGenre(response.data.food_genre);
+        setBlurb(response.data.blurb);
+        setStarAverage(response.data.starAverage);
+        setNumberOfReviews(response.data.number_of_review);
+        setOpenTime(response.data.open_time);
+        setCloseTime(response.data.close_time);
+        setOpenStatus(response.data.open_status);
+        setLatitude(response.data.latitude);
+        setLongitude(response.data.longitude);
+      })
+      .catch((err) => console.error(err));
+  };
 
   useEffect(() => {
-    const getData = async() => {
-      await axios.get(`${process.env.EXPO_LocalLan}/truck/login/${route.params.googleId}`)
-        .then((response) => {
-          setTruckName(response.data.full_name);
-          setPhoneNumber(response.data.phone_number);
-          setQrCode(response.data.qrCode);
-          setlogo(response.data.logo);
-          setFoodGenre(response.data.food_genre);
-          setBlurb(response.data.blurb);
-          setStarAverage(response.data.starAverage);
-          setNumberOfReviews(response.data.number_of_review);
-          setOpenTime(response.data.open_time);
-          setCloseTime(response.data.close_time);
-          setOpenStatus(response.data.open_status);
-          setLatitude(response.data.latitude);
-          setLongitude(response.data.longitude);
-        })
-        .catch((err) => console.error(err));
-    };
     getData();
   }, []);
+
+  useEffect(() => {
+    getData();
+  }, [isFocused]);
 
   const toggleSwitch = () => setOpenStatus((previousState) => !previousState);
 
