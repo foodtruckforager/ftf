3a5bd9808a0c9a7df1d50558c2682b243d5a6a78
Peter Klingelhofer <peterklingelhofer@gmail.com>
2020-09-17 17:17:41 -0500

    (add) posts and reviews are now more aesthetic, on cards from react-native-elements

--- client/components/dropIns/TruckReviewItem.tsx
+++ client/components/dropIns/TruckReviewItem.tsx
@@ -18,39 +18,44 @@ const TruckReviewItem = ({ review, currentTruck, currentTruckReviewers }) => {
   let userBadges = badge || '🎖';
   return (
     <View key={review.id} style={styles.container}>
-      <View style={styles.reviewHeader}>
-        <View style={styles.avatarNameBadge}>
-          <Avatar
-            rounded
-            source={{
-              uri: profilePhoto,
-            }}
-          />
-          <Text>{full_name}</Text>
-          <Text>{userBadges}</Text>
-        </View>
-        <Text style={styles.title}>{review_title}</Text>
-        <View style={styles.stars}>
-          <Text style={{ color: 'orange' }}>
-            {String.fromCharCode(9733).repeat(Math.floor(review_star))}
-          </Text>
-          <Text style={{ color: 'lightgrey' }}>
-            {String.fromCharCode(9733).repeat(5 - Math.floor(review_star))}
-          </Text>
-          <Text>{`${createdAt.substring(0, 10)} | ${createdAt.substring(
-            14,
-            19
-          )}`}</Text>
-        </View>
-      </View>
-      <Text>{review_description}</Text>
-      <Text>{`${upvotes} 👍`}</Text>
-      <Image
-        source={{ uri: reviewPhoto }}
-        style={styles.image}
-        resizeMode="contain"
-      />
-      <Divider style={{ backgroundColor: 'blue' }} />
+      <Card>
+          <View style={styles.reviewHeader}>
+            <View style={styles.avatarNameBadge}>
+              <Avatar
+                rounded
+                source={{
+                  uri: profilePhoto,
+                }}
+                />
+              <Text>{full_name}</Text>
+              <Text>{userBadges}</Text>
+            </View>
+              </View>
+                <Card.Title>
+            <Text style={styles.title}>{review_title}</Text>
+        </Card.Title>
+        <View style={styles.column}>
+            <View style={styles.stars}>
+              <Text style={{ color: 'orange' }}>
+                {String.fromCharCode(9733).repeat(Math.floor(review_star))}
+              </Text>
+              <Text style={{ color: 'lightgrey' }}>
+                {String.fromCharCode(9733).repeat(5 - Math.floor(review_star))}
+            </Text></View>
+            <View>
+              <Text>{`${createdAt.substring(0, 10)} | ${createdAt.substring(
+                14,
+                19
+              )}`}</Text></View>
+            </View>
+        <Card.Divider/>
+        <Text>{review_description}</Text>
+        <Text>{`${upvotes} 👍`}</Text>
+        <Card.Image
+          source={{ uri: reviewPhoto }}
+          resizeMode="cover"
+        />
+      </Card>
     </View>
   );
 };
@@ -61,10 +66,15 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
   },
-  reviewHeader: {
+  column: {
+    justifyContent: 'flex-start',
+    flexDirection: 'column',
     alignItems: 'center',
+  },
+  reviewHeader: {
+    // alignItems: 'center',
     flexShrink: 1,
-    flex: 0.4,
+    flex: 0.7,
   },
   stars: {
     flexDirection: 'row',
