d0fa85835961b4c8ea0a92e7af49e071d4c7181d
Peter Klingelhofer <peterklingelhofer@gmail.com>
2020-09-18 10:14:07 -0500

    (fix) info-window-post-review-details-fix for aesthetics and visibility

--- client/components/screens/truckReviews.tsx
+++ client/components/screens/truckReviews.tsx
@@ -142,7 +142,7 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
   },
   buttonsContainer: {
-    flex: 1,
+    flex: 2,
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
