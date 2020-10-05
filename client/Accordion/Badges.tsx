02dfe3db5290e2acf5d6cc4df684afa8893642b4
Jay Steigner <ioctosteigner@gmail.com>
2020-10-04 23:07:58 -0500

    (add) added tooltip for badges

--- client/Accordion/Badges.tsx
+++ client/Accordion/Badges.tsx
@@ -1,12 +1,103 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, TouchableWithoutFeedback } from 'react-native';
-
+import {
+  View, StyleSheet, Text, TouchableWithoutFeedback,
+} from 'react-native';
+import { Tooltip } from 'react-native-elements';
 import Animated, { Easing } from 'react-native-reanimated';
 import { bInterpolate, bin, useTransition } from 'react-native-redash';
 import Chevron from './Chevron';
 import Item, { LIST_ITEM_HEIGHT, ListItem } from './ListItem';
 
 const { not, interpolate } = Animated;
+
+export interface Badges {
+  name: string;
+  items: ListItem[];
+}
+
+interface ListProps {
+  badges: Badges;
+}
+
+export default ({ badges }: ListProps) => {
+  const [open, setOpen] = useState(false);
+  const transition = useTransition(
+    open,
+    not(bin(open)),
+    bin(open),
+    400,
+    Easing.inOut(Easing.ease),
+  );
+  const height = bInterpolate(
+    transition,
+    0,
+    LIST_ITEM_HEIGHT * badges.items.length,
+  );
+  const bottomRadius = interpolate(transition, {
+    inputRange: [0, 16 / 400],
+    outputRange: [8, 0],
+  });
+  return (
+    <>
+      <TouchableWithoutFeedback onPress={() => setOpen((prev) => !prev)}>
+        <Animated.View
+          style={[
+            styles.container,
+            {
+              borderBottomLeftRadius: bottomRadius,
+              borderBottomRightRadius: bottomRadius,
+            },
+          ]}
+        >
+          <Tooltip
+            containerStyle={styles.toolTipContainerStyle}
+            popover={(
+              <View>
+                <Text style={styles.toolTipPopover}>
+                  Scan Truck QR Codes to Earn Badges!
+                </Text>
+                <Text />
+                <Text style={styles.toolTipPopover}>
+                  <Text style={styles.toolTipBadgesTitle}>I Got Trucked</Text>
+                  - visit 3 trucks
+                </Text>
+                <Text style={styles.toolTipPopover}>
+                  <Text style={styles.toolTipBadgesTitle}>Parliament Truck-a-Delic</Text>
+                  - visit 10 trucks
+                </Text>
+                <Text style={styles.toolTipPopover}>
+                  <Text style={styles.toolTipBadgesTitle}>Feast Mode</Text>
+                  - visit 30 trucks
+                </Text>
+                <Text style={styles.toolTipPopover}>
+                  <Text style={styles.toolTipBadgesTitle}>Around The World</Text>
+                  - visit 5 different trucks
+                </Text>
+                <Text style={styles.toolTipPopover}>
+                  <Text style={styles.toolTipBadgesTitle}>Berserker</Text>
+                  - visit same truck 3x in 1 day
+                </Text>
+                <Text style={styles.toolTipPopover}>
+                  <Text style={styles.toolTipBadgesTitle}>The Regular</Text>
+                  - visit same truck 5x in 1 week
+                </Text>
+              </View>
+        )}
+          >
+            <Text style={styles.title}>Badges</Text>
+          </Tooltip>
+          <Chevron {...{ transition }} />
+        </Animated.View>
+      </TouchableWithoutFeedback>
+      <Animated.View style={[styles.items, { height }]}>
+        {badges.items.map((item, key) => (
+          <Item {...{ item, key }} isLast={key === badges.items.length - 1} />
+        ))}
+      </Animated.View>
+    </>
+  );
+};
+
 const styles = StyleSheet.create({
   container: {
     marginTop: 16,
@@ -25,56 +116,14 @@ const styles = StyleSheet.create({
   items: {
     overflow: 'hidden',
   },
+  toolTipContainerStyle: {
+    width: 300,
+    height: 150,
+  },
+  toolTipPopover: {
+    color: 'white',
+  },
+  toolTipBadgesTitle: {
+    textDecorationLine: 'underline',
+  },
 });
-
-export interface Badges {
-  name: string;
-  items: ListItem[];
-}
-
-interface ListProps {
-  badges: Badges;
-}
-
-export default ({ badges }: ListProps) => {
-  const [open, setOpen] = useState(false);
-  const transition = useTransition(
-    open,
-    not(bin(open)),
-    bin(open),
-    400,
-    Easing.inOut(Easing.ease)
-  );
-  const height = bInterpolate(
-    transition,
-    0,
-    LIST_ITEM_HEIGHT * badges.items.length
-  );
-  const bottomRadius = interpolate(transition, {
-    inputRange: [0, 16 / 400],
-    outputRange: [8, 0],
-  });
-  return (
-    <>
-      <TouchableWithoutFeedback onPress={() => setOpen((prev) => !prev)}>
-        <Animated.View
-          style={[
-            styles.container,
-            {
-              borderBottomLeftRadius: bottomRadius,
-              borderBottomRightRadius: bottomRadius,
-            },
-          ]}
-        >
-          <Text style={styles.title}>Badges</Text>
-          <Chevron {...{ transition }} />
-        </Animated.View>
-      </TouchableWithoutFeedback>
-      <Animated.View style={[styles.items, { height }]}>
-        {badges.items.map((item, key) => (
-          <Item {...{ item, key }} isLast={key === badges.items.length - 1} />
-        ))}
-      </Animated.View>
-    </>
-  );
-};
